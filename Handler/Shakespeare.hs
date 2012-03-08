1193ee1f3331ef8ea67f473999d2d9dfb6cfa0c2
melpon <melt@e-ml.jp>
2012-03-09 00:14:26 +0900

    ドキュメント更新

--- Handler/Shakespeare.hs
+++ Handler/Shakespeare.hs
@@ -86,3 +86,158 @@ renderMyRoute :: Render MyRoute
 renderMyRoute Home _ = ...
 renderMyRoute Time _ = ...
 |]
+
+code9 :: String
+code9 = [rawstring|
+<a href=@{Time}>The time
+|]
+
+code10 :: String
+code10 = [rawstring|
+\render -> mconcat ["<a href='", render Time, "'>The time</a>"]
+|]
+
+code11 :: String
+code11 = [rawstring|
+<body>
+<p>Some paragraph.</p>
+<ul>
+<li>Item 1</li>
+<li>Item 2</li>
+</ul>
+</body>
+|]
+
+code12 :: String
+code12 = [rawstring|
+<body>
+    <p>Some paragraph.
+    <ul>
+        <li>Item 1
+        <li>Item 2
+|]
+
+code13 :: String
+code13 = [rawstring|
+<p>Paragraph <i>italic</i> end.</p>
+|]
+
+code14 :: String
+code14 = [rawstring|
+<p>
+    Paragraph #
+    <i>italic
+    \ end.
+|]
+
+code15 :: String
+code15 = [rawstring|
+<p>Paragraph <i>italic</i> end.
+|]
+
+code16 :: String
+code16 = [rawstring|
+<head>
+    <title>#{title}
+|]
+
+code17 :: String
+code17 = [rawstring|
+-- shamlet による準クォートについては今は無視する。
+-- それは後で説明する。
+{-# LANGUAGE QuasiQuotes #-}
+import Text.Hamlet (shamlet)
+import Text.Blaze.Renderer.String (renderHtml)
+import Data.Char (toLower)
+import Data.List (sort)
+
+data Person = Person
+    { name :: String
+    , age  :: Int
+    }
+
+main :: IO ()
+main = putStrLn $ renderHtml [shamlet|
+<p>Hello, my name is #{name person} and I am #{show $ age person}.
+<p>
+    Let's do some funny stuff with my name: #
+    <b>#{sort $ map toLower (name person)}
+<p>Oh, and in 5 years I'll be #{show $ (+) 5 (age person)} years old.
+\|]
+  where
+    person = Person "Michael" 26
+|]
+
+code18 :: String
+code18 = [rawstring|
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+import Text.Hamlet (HtmlUrl, hamlet)
+import Text.Blaze.Renderer.String (renderHtml)
+import Data.Text (Text)
+
+data MyRoute = Home
+
+render :: MyRoute -> [(Text, Text)] -> Text
+render Home _ = "/home"
+
+footer :: HtmlUrl MyRoute
+footer = [hamlet|
+<footer>
+    Return to #
+    <a href=@{Home}>Homepage
+    .
+\|]
+
+main :: IO ()
+main = putStrLn $ renderHtml $ [hamlet|
+<body>
+    <p>This is my page.
+    ^{footer}
+\|] render
+|]
+
+code19 :: String
+code19 = [rawstring|
+$if isAdmin
+    <p>Welcome to the admin section.
+$elseif isLoggedIn
+    <p>You are not the administrator.
+$else
+    <p>I don't know who you are. Please log in so I can decide if you get access.
+|]
+
+code20 :: String
+code20 = [rawstring|
+$maybe name <- maybeName
+    <p>Your name is #{name}
+$nothing
+    <p>I don't know your name.
+|]
+
+code21 :: String
+code21 = [rawstring|
+$if null people
+    <p>No people.
+$else
+    <ul>
+        $forall person <- people
+            <li>#{person}
+|]
+
+code22 :: String
+code22 = [rawstring|
+$with foo <- (一度だけ実行されるべき、とても長くて汚い式)
+    <p>しかし私は #{foo} を何度も使うだろう。#{foo}
+|]
+
+code23 :: String
+code23 = [rawstring|
+!!!
+<html>
+    <head>
+        <title>Hamlet is Awesome
+    <body>
+        <p>All done.
+|]
+
